3457fe67f9a935cc915814f71bfc76fb7b7d69ca
Michael Bubienczyk <michaelbub@outlook.com>
2025-08-26 18:42:44 +0300

    Refactor list row to avoid type-checking timeout

--- Services/Haptics.swift
+++ Services/Haptics.swift
@@ -4,6 +4,9 @@ enum Haptics {
     static func light() {
         UIImpactFeedbackGenerator(style: .light).impactOccurred()
     }
+    static func medium() {
+        UIImpactFeedbackGenerator(style: .medium).impactOccurred()
+    }
     static func success() {
         UINotificationFeedbackGenerator().notificationOccurred(.success)
     }
