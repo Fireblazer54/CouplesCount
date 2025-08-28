56b16a036904f5c8097316fed0fb977dbf31d2b6
Michael Bubienczyk <michaelbub@outlook.com>
2025-08-28 13:47:15 +0200

    Use transparent list row background

--- CouplesCount/Views/Countdowns/CountdownRowView.swift
+++ CouplesCount/Views/Countdowns/CountdownRowView.swift
@@ -50,7 +50,7 @@ struct CountdownRowView: View {
         }
         .listRowSeparator(.hidden)
         .listRowInsets(.init(top: 8, leading: 16, bottom: 8, trailing: 16))
-        .listRowBackground(Color.white)
+        .listRowBackground(Color.clear)
         .swipeActions(edge: .trailing, allowsFullSwipe: false) {
             DeleteSwipeButton({ onDelete(countdown) }, background: Color("Destructive"), foreground: .white)
         }
