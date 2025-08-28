e462a179ab68804f60fe71b663eb69762b5ec543
Michael Bubienczyk <michaelbub@outlook.com>
2025-08-28 14:58:39 +0200

    Remove header top padding and ignore safe area

--- CouplesCount/Views/Countdowns/CountdownListView.swift
+++ CouplesCount/Views/Countdowns/CountdownListView.swift
@@ -27,6 +27,7 @@ struct CountdownListView: View {
 
                 VStack(spacing: 0) {
                     HeaderView(showPaywall: $showPaywall, showSettingsPage: $showSettingsPage)
+                        .ignoresSafeArea(edges: .top)
 
 
                     if items.isEmpty {
@@ -117,7 +118,6 @@ private struct HeaderView: View {
             }
         }
         .padding(.horizontal)
-        .padding(.top, 8)
         .padding(.bottom, 8)
         .background(Color.white)
         .shadow(color: .black.opacity(0.05), radius: 4, y: 2)
