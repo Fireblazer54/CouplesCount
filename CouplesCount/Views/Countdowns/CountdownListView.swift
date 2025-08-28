3e966b2c7eec269e6b20a5790b88c3e62218dc40
Michael Bubienczyk <michaelbub@outlook.com>
2025-08-28 13:04:49 +0200

    Switch to tinted swipe actions

--- CouplesCount/Views/Countdowns/CountdownListView.swift
+++ CouplesCount/Views/Countdowns/CountdownListView.swift
@@ -202,7 +202,6 @@ private struct CountdownListSection: View {
                             let all = (try? modelContext.fetch(FetchDescriptor<Countdown>())) ?? []
                             updateWidgetSnapshot(afterSaving: all)
                         }
-                        Haptics.warning()
                     },
                     onArchiveToggle: { countdown in
                         withAnimation(.spring(response: 0.4, dampingFraction: 0.85)) {
@@ -211,7 +210,6 @@ private struct CountdownListSection: View {
                             let all = (try? modelContext.fetch(FetchDescriptor<Countdown>())) ?? []
                             updateWidgetSnapshot(afterSaving: all)
                         }
-                        if countdown.isArchived { Haptics.light() }
                     },
                     onSelect: { countdown in
                         selectedCountdown = countdown
